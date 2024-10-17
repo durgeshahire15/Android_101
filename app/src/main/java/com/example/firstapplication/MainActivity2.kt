8f0e8a5fccc0d8c2d608e711deb55905e8c0791b
Durgesh Ahire <durgeshahire15@gmail.com>
2024-10-18 02:52:40 +0530

    Used constraint layout

---
+++ app/src/main/java/com/example/firstapplication/MainActivity2.kt
@@ -0,0 +1,28 @@
+package com.example.firstapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button1 = findViewById<Button>(R.id.button1)
+
+        button1.setOnClickListener {
+            Toast.makeText(this, "Hi, this is my first project", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
