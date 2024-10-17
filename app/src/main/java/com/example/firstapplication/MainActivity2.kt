79eb2404eb68615a4d4be52274122b3c2fa3d0e2
Durgesh Ahire <durgeshahire15@gmail.com>
2024-10-18 03:07:31 +0530

    Added default Alert dialog box

--- app/src/main/java/com/example/firstapplication/MainActivity2.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity2.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -23,6 +24,23 @@ class MainActivity2 : AppCompatActivity() {
 
         button1.setOnClickListener {
             Toast.makeText(this, "Hi, this is my first project", Toast.LENGTH_SHORT).show()
+        }
+
+        val button2 = findViewById<Button>(R.id.button8)
+        button2.setOnClickListener{
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Delete it")
+            alert.setMessage("Do want to delete this?")
+            alert.setPositiveButton("Yes"){dialogInterface, which->
+                Toast.makeText(this, "You got it deleted",Toast.LENGTH_SHORT).show()
+            }
+            alert.setNegativeButton("No"){dialogInterface, which->
+                Toast.makeText(this, "You got it deleted",Toast.LENGTH_SHORT).show()
+            }
+            alert.setNeutralButton("cancel"){dialogInterface, which->
+                Toast.makeText(this, "You got it deleted",Toast.LENGTH_SHORT).show()
+            }
+            alert.show()
         }
     }
 }
