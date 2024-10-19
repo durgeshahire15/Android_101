f57c992ff1bd1706f4e8a09cfae7136406d9f150
Durgesh Ahire <durgeshahire15@gmail.com>
2024-10-19 17:48:27 +0530

    inflating layout via backend and not xml file

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -5,17 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.firstapplication.databinding.ActivityMainBinding
+import com.example.firstapplication.fragments.BlankFragment1
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var manager = supportFragmentManager
+        var tr = manager.beginTransaction()
+        tr.replace(R.id.frameLayout, BlankFragment1())
+        tr.commit()
+
     }
     override fun onResume(){
         super.onResume()
