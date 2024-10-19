43e54287dbe2f1c4939677f372978e7ed9f6a0de
Durgesh Ahire <durgeshahire15@gmail.com>
2024-10-19 16:41:19 +0530

    Used explicit intent

--- app/src/main/java/com/example/firstapplication/ProfileActivity.kt
+++ app/src/main/java/com/example/firstapplication/ProfileActivity.kt
@@ -1,34 +1,20 @@
 package com.example.firstapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val profButton = findViewById<Button>(R.id.profileButton)
-
-        profButton.setOnClickListener {
-            val intent = Intent(this, ProfileActivity::class.java)
-            startActivity(intent)
-        }
     }
-    override fun onResume(){
-        super.onResume()
-
-    }
-
-
 }
